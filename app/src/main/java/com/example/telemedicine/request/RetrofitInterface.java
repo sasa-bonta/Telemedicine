e5dd2d39ca32480ba8a9554257181f8d9c7c0bcf
Alex Caragia <allexandrucaragia@gmail.com>
2019-12-13 18:57:02 +0200

    TODO: UI

--- app/src/main/java/com/example/telemedicine/request/RetrofitInterface.java
+++ app/src/main/java/com/example/telemedicine/request/RetrofitInterface.java
@@ -3,13 +3,31 @@ package com.example.telemedicine.request;
 import com.example.telemedicine.model.NewUserResponse;
 
 import io.reactivex.Single;
+import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface RetrofitInterface {
 
-    @FormUrlEncoded
-    @POST("~/api/Login/UserAuth")
-    Single<NewUserResponse> register();
+    //http://81.180.72.17/
 
+    @Headers({
+            "Content-Type:application/x-www-form-urlencoded"
+    })
+
+    @FormUrlEncoded
+    @POST("api/Register/UserReg")
+    Single<NewUserResponse> registerUser(@Field("FullName") String fullName,
+                                         @Field("Birthday") String birthday,
+                                         @Field("Email") String email,
+                                         @Field("Phone") String phone,
+                                         @Field("Address") String address,
+                                         @Field("Username") String username,
+                                         @Field("Password") String password,
+                                         @Field("Base64Photo") String base64Photo);
+
+    /*@FormUrlEncoded
+    @POST("/api/Login/UserAuth")
+    Single<>*/
 }
